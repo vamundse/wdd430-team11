14c4bcdad9fbbfdd3f1e96a3432520dbdfc9395f
vamundse <vamundse@gmail.com>
2026-09-10 15:05:12 +0200

    I have made a basic folder structure, basic pages and a basic look for the app.

---
+++ app/subjects/page.tsx
@@ -0,0 +1,7 @@
+export default function Subjects() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen w-full bg-gray-100">
+            <p className="text-lg text-gray-700">This is the Subjects page.</p>
+        </div>
+    )
+}
