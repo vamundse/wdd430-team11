bd11854015cdf412a4818192dc60b5a5331ab913
KishieBhenyu <kbhenyu@byupathway.edu>
2026-09-22 17:11:58 +0200

    footer

--- app/subjects/page.tsx
+++ app/subjects/page.tsx
@@ -1,7 +1,7 @@
 export default function Subjects() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen w-full bg-gray-100">
-            <p className="text-lg text-gray-700">This is the Subjects page.</p>
-        </div>
-    )
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center w-full bg-gray-100">
+      <p className="text-lg text-gray-700">This is the Subjects page.</p>
+    </div>
+  );
 }
